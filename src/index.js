b5507ce26362ad3b549a8423995fdf832da9d83a
CPPtrueCoder <andierodin@gmail.com>
2020-01-16 16:16:46 +0300

    Add components for drag and drop

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import initial from "./initial-data";
+import {DragDropContext} from "react-beautiful-dnd";
+
+import Column from "./column";
+class App extends Component {
+    state = initial;
+onDragEnd = result=>{
+    //Todo reorder
+}
+    render() {
+
+        return(
+            <DragDropContext
+
+            onDragEnd={this.onDragEnd}
+
+            >
+                { this.state.columnOrder.map((columnId) => {
+            const column = this.state.columns[columnId];
+            const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+            return <Column key={columnId} column={column} tasks={tasks}/>;
+            })}
+            </DragDropContext>
+
+        );
+
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
